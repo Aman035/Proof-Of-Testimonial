afe0d46068da5ebea4a2a2b4cba85ea037cef7bf
aman035 <guptaaman200115@gmail.com>
2024-04-08 21:57:08 +0530

    add: inital setup

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Navbar } from './components'
+import { Home, Products, NotFound } from './pages'
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
