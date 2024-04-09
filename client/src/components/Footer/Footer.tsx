f7b1792ddff443b30b0b15981387b47f91ae274a
aman035 <guptaaman200115@gmail.com>
2024-04-10 00:31:39 +0530

    add: footer comp

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="text-gray-400 py-4 text-center">
+      <h2 className="text-base leading-7">
+        © 2024 Aman, Inc. All rights reserved.
+      </h2>
+    </footer>
+  )
+}
+
+export default Footer
