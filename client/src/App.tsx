d80ce98a492dd339c04dae04b7feb78483d302d5
aman035 <guptaaman200115@gmail.com>
2024-04-10 01:01:21 +0530

    fix: page takes min height & scroll=0 on route switch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,39 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom'
 import { Footer, Navbar } from './components'
 import { Home, Products, NotFound } from './pages'
+import { useEffect } from 'react'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null // This component does not render anything
+}
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop /> {/* Place ScrollToTop inside Router */}
       <Navbar />
-      <div className="flex flex-col h-screen">
-        <main className="flex-grow">
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-grow">
           <Routes>
+            {' '}
+            {/* Main content grows to take available space */}
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-          <Footer />
-        </main>
+        </div>
+        <Footer />
       </div>
     </Router>
   )
